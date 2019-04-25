1cd63d35c66a4fbbc924c6b8a5244f2594e864f7
Khalil Jomaa <khjomaa@gmail.com>
2019-04-25 18:35:04 +0300

    added clock function

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from time import strftime, sleep
-   
-while True:
-    try:
-        print(strftime("%H:%M:%S"), end='\r')
-        sleep(1)
-    except KeyboardInterrupt as e:
-        print("", end='\r', flush=True)
-        print("Bye Bye...")
-        exit(0)
+
+def start_clock():
+    while True:
+        try:
+            print(strftime("%H:%M:%S"), end='\r')
+            sleep(1)
+        except KeyboardInterrupt as e:
+            print("", end='\r', flush=True)
+            print("Bye Bye...")
+            exit(0)
+
+start_clock()
